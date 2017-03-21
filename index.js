1cee995b258700dd29da9193cfd76073d3f713c0
rmody3 <rmody3@gmail.com>
2017-03-21 13:34:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-const app = "I don't do much."
+
+
+function Sandwich(bread, ingredients, cut) {
+  this.bread = bread;
+  this.ingredients = ingredients;
+  this.cut = cut;
+  this.serve = function() {
+    console.log("here's your " + this.bread + ", enjoy!");
+  }
+}
+
+var blt = new Sandwich("white", ["bacon", "lettuce", "tomato", "mayo"], "rectangle")
+var reuben = new Sandwich("rye", ["corned beef", "sauerkraut", "swiss"], "diagonal"])
